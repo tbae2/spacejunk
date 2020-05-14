3eb78396f8219b19af0524e96d90b0c87279da32
Thomas <thomas.bae0@gmail.com>
2020-05-14 16:38:23 -0400

    updating conf to ini , add config parsing , implement spactrack api, initial box score grab test

--- datatasks/summaryreports.py
+++ datatasks/summaryreports.py
@@ -1,11 +1,20 @@
 from pprint import pprint
 from space_track_api import SpaceTrackApi
+import configparser
+import json
 
 
 
 def main():
     pass
-
+    config = configparser.ConfigParser()
+    config.read('../configs/config.ini')
+    spacetrack = config['spacetrack']
+    st_login = spacetrack['login']
+    st_password = spacetrack['password']
+    with SpaceTrackApi(login=st_login, password=st_password) as stapi:
+        country_score = stapi.boxscore()
+        print(country_score)
 
 
 if __name__ == '__main__':
