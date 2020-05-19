14c29aedcd630aee876fdd75e4daab7739887084
Thomas <thomas.bae0@gmail.com>
2020-05-19 12:09:44 -0400

    converting methods to class methods

--- datatasks/summaryreports.py
+++ datatasks/summaryreports.py
@@ -6,18 +6,26 @@ import json
 
 
 def main():
-    pass
-    config = configparser.ConfigParser()
-    config.read('../configs/config.ini')
-    spacetrack = config['spacetrack']
-    st_login = spacetrack['login']
-    st_password = spacetrack['password']
-    with SpaceTrackApi(login=st_login, password=st_password) as stapi:
-        country_score = stapi.boxscore()
-        pprint(country_score, indent=2)
-        with open('../datastore/country_score.json','w') as score_file:
-            score_file.write(json.dumps(country_score))
+    pass    
+   
+class space_track():
+
+    def __init__(self):
+        self.st_config = configparser.ConfigParser()
+        self.st_config.read('../configs/config.ini')
+        self.st_config = self.st_config['spacetrack']
+        self.st_login = self.st_config['login']
+        self.st_password = self.st_config['password']
     
+    def dailyScore(self):
+        with SpaceTrackApi(login=self.st_login,password=self.st_password) as stapi:
+            country_score = stapi.boxscore()
+            pprint(country_score, indent=2)
+            with open('../datastore/country_score.json','w') as score_file:
+                score_file.write(json.dumps(country_score))
+    
+    def tle_dump(self):
+        pass
 
 if __name__ == '__main__':
     main()
