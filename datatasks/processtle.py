e7c6f4eca6cc7c2b668ead0d2b8fcd9ce01c373e
Thomas <thomas.bae0@gmail.com>
2020-05-22 15:27:07 -0400

    produce latitude / longitude degrees instead

--- datatasks/processtle.py
+++ datatasks/processtle.py
@@ -1,5 +1,6 @@
 from skyfield.api import EarthSatellite,Topos,load
 import json
+import time
 
 
 def processTle():
@@ -7,14 +8,17 @@ def processTle():
     with open("../datastore/tle_data.json") as tle_data:
         data = json.load(tle_data)
         ts = load.timescale()
+        calc_time = ts.now()
         for tle in data:
             tle_line1 = tle["TLE_LINE1"]
             tle_line2 = tle["TLE_LINE2"]
             name = tle["OBJECT_NAME"]
             satellite = EarthSatellite(tle_line1,tle_line2,name,ts)
+            geocentric = satellite.at(calc_time)
+            subpoint = geocentric.subpoint()
+            print('Latitude:', subpoint.latitude.degrees)
+            print('Longitude:', subpoint.longitude.degrees)
             print(satellite)
-            print(len(data))    
-            print(tle["NORAD_CAT_ID"])
-            print(tle["ORDINAL"])
+
 
 
