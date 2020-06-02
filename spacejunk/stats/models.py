300dbf712d22a1211884c339183a13a60669f2a5
Thomas <thomas.bae0@gmail.com>
2020-06-02 14:31:55 -0400

    stubbing model classes

--- spacejunk/stats/models.py
+++ spacejunk/stats/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class sattelite(models.Model):
+    pass
+
+class launchSite(models.Model):
+    pass
+
+class countryScore(models.Model):
+    pass
