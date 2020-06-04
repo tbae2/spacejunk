7965ec518cf348b0f7bfa996a9276f74fad978f7
Thomas <thomas.bae0@gmail.com>
2020-06-04 11:21:30 -0400

    modifying and adding country_sat view for import

--- spacejunk/stats/views.py
+++ spacejunk/stats/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.conf import settings
 import os
-from .models import LaunchSite
+from .models import LaunchSite,CountryScore
 import json as js
 from django.http import HttpResponse
 
 # Create your views here.
 
 def importLaunchSite(request):
+    ## function that can be called via url to import the launch_site data into the database ##
     file_path = os.path.join(settings.DATA_STORE,'launch_site_data.json')
     with open(file_path) as ls_data:
         data = js.load(ls_data)
@@ -26,3 +27,30 @@ def importLaunchSite(request):
 
             obj_result.append(site_result)
         return HttpResponse(obj_result)
+
+def importCountryScore(request):
+    ### function that can be called via url to import the country score file to db ###
+    file_path = os.path.join(settings.DATA_STORE,'country_score.json')
+    with open(file_path) as country_stats:
+        data = js.load(country_stats)
+
+        obj_result = []
+        for country in data:
+            country_stat = {
+                "country": country["COUNTRY"],
+                "record": None,
+                "created": False
+            }
+        obj,created = CountryScore.objects.get_or_create(
+            launch_country = country["COUNTRY"],
+            spadoc_cd = country["SPADOC_CD"],
+            orbital_tba = country["ORBITAL_TBA"],
+            orbital_payload_count = country["ORBITAL_PAYLOAD_COUNT"],
+            orbital_total_count = country["ORBITAL_TOTAL_COUNT"],
+            decayed_total_count = country["DECAYED_TOTAL_COUNT"],
+            country_total = country["COUNTRY_TOTAL"],
+        )
+        country_stat["record"] = obj
+        country_stat["created"] = created
+        obj_result.append(country_stat)
+    return HttpResponse(obj_result)
