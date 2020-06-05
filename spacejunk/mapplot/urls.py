c9247fdb5b7dc76d31876b517c0513e04de17d69
Thomas <thomas.bae0@gmail.com>
2020-06-05 17:30:47 -0400

    changing index to class based view , wrapping head around vue.js

--- spacejunk/mapplot/urls.py
+++ spacejunk/mapplot/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'mapplot'
 urlpatterns= [
-    path('', views.index, name='index'),
+    path('', views.MapPlotView.as_view(), name='index'),
     path('api/importtle/',views.importTle, name='load-tle-data'),
     path('api/importsatellites/',views.importSatellites, name='load-sat-data')
 ]
