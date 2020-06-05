ce334e002aef6d38f7975f348358a6a323f88fb6
Thomas <thomas.bae0@gmail.com>
2020-06-05 12:37:17 -0400

    upping max field length in model, update importtle to work, update to proper spacetrack api pip package in requirements.txt

--- spacejunk/mapplot/views.py
+++ spacejunk/mapplot/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.conf import settings
 import os
 import json as js
+import datetime
 from .models import tle_record,satellite
 # Create your views here.
 
@@ -20,9 +21,9 @@ def importTle(request):
 
     with open(file_path,'r') as tle_data:
         data = js.load(tle_data)
-
         obj_result = []
-        for tle in tle_data:
+        
+        for tle in data:
             tle_stat = {
                 "norad_cat_id": tle["NORAD_CAT_ID"],
                 "object_name": tle["OBJECT_NAME"],
@@ -31,22 +32,22 @@ def importTle(request):
                 "created": False
             }
             obj,created = tle_record.objects.get_or_create(
-                norad_cat_id        = tle_record["NORAD_CAT_ID"],
-                object_name         = tle_record["OBJECT_NAME"],
-                object_type         = tle_record["OBJECT_TYPE"],
-                object_id           = tle_record["OBJECT_ID"],
-                classification_type = tle_record["CLASSIFICATION_TYPE"],
-                intldes             = tle_record["INTLDES"],
-                epoch               = tle_record["EPOCH"],
-                epoch_microseconds  = tle_record["EPOCH_MICROSECONDS"],
-                tle1                = tle_record["TLE_LINE1"],
-                tle2                = tle_record["TLE_LINE2"],
-                period              = tle_record["PERIOD"],
-                apogee              = tle_record["APOGEE"],
-                perigee             = tle_record["PERIGEE"],
-                decayed             = tle_record["DECAYED"],
-                latitude            = tle_record["Latitude"],
-                longitude           = tle_record["Longitude"],
+                norad_cat_id        = tle["NORAD_CAT_ID"],
+                object_name         = tle["OBJECT_NAME"],
+                object_type         = tle["OBJECT_TYPE"],
+                object_id           = tle["OBJECT_ID"],
+                classification_type = tle["CLASSIFICATION_TYPE"],
+                intldes             = tle["INTLDES"],
+                epoch               = tle["EPOCH"],
+                epoch_microseconds  = tle["EPOCH_MICROSECONDS"],
+                tle1                = tle["TLE_LINE1"],
+                tle2                = tle["TLE_LINE2"],
+                period              = tle["PERIOD"],
+                apogee              = tle["APOGEE"],
+                perigee             = tle["PERIGEE"],
+                decayed             = tle["DECAYED"],
+                latitude            = tle["Latitude"],
+                longitude           = tle["Longitude"],
             )
             tle_stat["created"] = created
             obj_result.append(tle_stat)
