298134e3848008cc1db93842340fa1b114068a72
Thomas <thomas.bae0@gmail.com>
2020-06-09 15:40:10 -0400

    adding base template, adding stats base url, and base class view

--- spacejunk/stats/urls.py
+++ spacejunk/stats/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('',views.StatsView.as_view(),name='stats'),
     path('api/loadlaunchsites',views.importLaunchSite,name='load-launch-site'),
     path('api/loadcountryscore',views.importCountryScore,name='load-country-score')
 ]
